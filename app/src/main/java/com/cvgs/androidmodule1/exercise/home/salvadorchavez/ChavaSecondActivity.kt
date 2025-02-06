44b5141c2e8e35799ab40a45cb6f0385d5ca04d4
Chava <dchavezvillanueva@gmail.com>
2025-02-05 21:38:45 -0600

    Deleted unnecessary imports

--- app/src/main/java/com/cvgs/androidmodule1/exercise/home/salvadorchavez/ChavaSecondActivity.kt
+++ app/src/main/java/com/cvgs/androidmodule1/exercise/home/salvadorchavez/ChavaSecondActivity.kt
@@ -5,13 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cvgs.androidmodule1.R
-import java.time.LocalDate
 import java.util.Calendar
 
 class ChavaSecondActivity : AppCompatActivity() {
