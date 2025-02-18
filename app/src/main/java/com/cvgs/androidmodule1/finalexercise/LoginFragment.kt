83c4a5051e47d553fd5a19152f1f54909111ba39
IvanUsiel <usiel_jarquin@outlook.com>
2025-02-18 12:19:10 -0600

    Subo mi parte del PY Final

--- app/src/main/java/com/cvgs/androidmodule1/finalexercise/LoginFragment.kt
+++ app/src/main/java/com/cvgs/androidmodule1/finalexercise/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.cvgs.androidmodule1.finalexercise
 
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
@@ -12,59 +13,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity.RESULT_OK
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.cvgs.androidmodule1.R
 import com.cvgs.androidmodule1.databinding.FragmentLoginBinding
-import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.ChavaSignUpActivity
 import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.isNotFilled
 import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.markAsInvalid
 import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.resetOnTyping
 
 class LoginFragment : Fragment() {
     private lateinit var binding : FragmentLoginBinding
-    lateinit var register: ActivityResultLauncher<Intent>
-    var registeredEmail: String? = null
-    var registeredPassword: String? = null
-    var registeredGender: String? = null
-    var registeredName: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         /********************************
          *          Variables
          * **********************************/
         val spannableString = SpannableString("¿No tienes cuenta? Crear cuenta") //Para poder crear cuenta en texto
-        register = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if (result.resultCode == RESULT_OK){
-                registeredEmail = result.data?.getStringExtra("EXTRA_EMAIL")
-                registeredPassword = result.data?.getStringExtra("EXTRA_PASSWORD")
-                registeredGender = result.data?.getStringExtra("EXTRA_SEX")
-                registeredName = result.data?.getStringExtra("EXTRA_NAME")
-                //Toast.makeText(this,"isCorrect: $isCorrect", Toast.LENGTH_SHORT).show()
-            }
-        }
+        //val registeredUser = arguments?.getSerializable("EXTRA_USER") as? UserEntity
+        val registeredUser = arguments?.getSerializable("EXTRA_USER", UserEntity::class.java)
         /********************************************
-        Span para Intent de sign up
+        Span para Fragment de sign up
          **********************************************/
         val clickableSpan = object : ClickableSpan() {
             override fun onClick(widget: View) {
-                /*parentFragmentManager.beginTransaction()
+                parentFragmentManager.beginTransaction()
                     .replace(R.id.fragmentContainer, SignUpFragment.newInstance())
                     .addToBackStack("SignUpFragment")
-                    .commit()*/
+                    .commit()
             }
 
             override fun updateDrawState(ds: TextPaint) {
@@ -78,9 +63,6 @@ class LoginFragment : Fragment() {
         binding.tvSignUp.text = spannableString
         binding.tvSignUp.movementMethod = LinkMovementMethod.getInstance()
 
-        /*************************************
-        Listeners
-         ************************************/
         binding.etEmail.resetOnTyping()
         binding.etPassword.resetOnTyping()
 
@@ -97,23 +79,76 @@ class LoginFragment : Fragment() {
 
             if(isValid){
                 //Inicia sesion
-                if (binding.etPassword.text.toString() == registeredPassword &&
-                    binding.etEmail.text.toString() == registeredEmail){
-                    if(registeredGender == "Hombre"){
-                        Toast.makeText(requireContext(), "Bienvenido, $registeredName", Toast.LENGTH_SHORT).show()
-                    }else if(registeredGender == "Mujer"){
-                        Toast.makeText(requireContext(), "Bienvenida, $registeredName", Toast.LENGTH_SHORT).show()
-                    }else if(registeredGender == "Otro"){
-                        Toast.makeText(requireContext(), "Bienvenide, $registeredName", Toast.LENGTH_SHORT).show()
+                if (binding.etPassword.text.toString() == registeredUser?.password  &&
+                    binding.etEmail.text.toString() == registeredUser?.email){
+                    if(registeredUser?.gender == "Hombre"){
+                        //Toast.makeText(requireContext(), "Bienvenido, ${registeredUser?.name}", Toast.LENGTH_SHORT).show()
+                        val message = """${registeredUser?.name}""".trimIndent()
+                        val alertDialog = AlertDialog.Builder(requireContext())
+                            .setTitle("Bienvenido")
+                            .setMessage(message)
+                            .setPositiveButton("Ingresar") { dialog, which ->
+                                val intent = Intent(requireContext(), StartUpActivity::class.java).apply {
+                                    putExtra("EXTRA_USER", registeredUser)
+                                }
+                                startActivity(intent)
+                            }
+                            .create()
+                        alertDialog.show()
+
+                    }else if(registeredUser?.gender == "Mujer"){
+                        //Toast.makeText(requireContext(), "Bienvenida, ${registeredUser?.name}", Toast.LENGTH_SHORT).show()
+                        val message = """${registeredUser?.name}""".trimIndent()
+                        val alertDialog = AlertDialog.Builder(requireContext())
+                            .setTitle("Bienvenida")
+                            .setMessage(message)
+                            .setPositiveButton("Ingresar") { dialog, which ->
+                                val intent = Intent(requireContext(), StartUpActivity::class.java).apply {
+                                    putExtra("EXTRA_USER", registeredUser)
+                                }
+                                startActivity(intent)
+                            }
+                            .create()
+                        alertDialog.show()
+                    }else if(registeredUser?.gender == "Otro"){
+                        //Toast.makeText(requireContext(), "Bienvenide, ${registeredUser?.name}", Toast.LENGTH_SHORT).show()
+                        val message = """${registeredUser?.name}""".trimIndent()
+                        val alertDialog = AlertDialog.Builder(requireContext())
+                            .setTitle("Bienvenide")
+                            .setMessage(message)
+                            .setPositiveButton("Ingresar") { dialog, which ->
+                                val intent = Intent(requireContext(), StartUpActivity::class.java).apply {
+                                    putExtra("EXTRA_USER", registeredUser)
+                                }
+                                startActivity(intent)
+                            }
+                            .create()
+                        alertDialog.show()
+
                     }
 
                 }else{
-                    Toast.makeText(requireContext(), "Correo o contraseña equivocados o no registrados", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(requireContext(), "Correo o contraseña equivocados o no registrados", Toast.LENGTH_SHORT).show()
+                    val message = """Correo o contraseña equivocados o no registrados""".trimIndent()
+                    val alertDialog = AlertDialog.Builder(requireContext())
+                        .setTitle("Ups!")
+                        .setMessage(message)
+                        .setPositiveButton("OK") { dialog, which -> }
+                        .create()
+                    alertDialog.show()
                 }
 
 
             }else{
-                Toast.makeText(requireContext(),"Ingresa los datos faltantes", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(requireContext(),"Ingresa los datos faltantes", Toast.LENGTH_SHORT).show()
+                val message = """Ingresa los datos faltantes""".trimIndent()
+                val alertDialog = AlertDialog.Builder(requireContext())
+                    .setTitle("Ups!")
+                    .setMessage(message)
+                    .setPositiveButton("OK") { dialog, which -> }
+                    .create()
+                alertDialog.show()
+
             }
         }
     }
---
+++ app/src/main/java/com/cvgs/androidmodule1/finalexercise/UserEntity.kt
@@ -0,0 +1,13 @@
+package com.cvgs.androidmodule1.finalexercise
+
+import java.io.Serializable
+
+data class UserEntity(
+    val name: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    val gender: String
+) : Serializable
+
+
