5e8478469a28d54912864ab93b952558aedac84c
ferlg <ferlory17@gmail.com>
2025-02-21 15:32:30 -0600

    modificacion strings

--- app/src/main/java/com/cvgs/androidmodule1/finalexercise/SignUpFragment.kt
+++ app/src/main/java/com/cvgs/androidmodule1/finalexercise/SignUpFragment.kt
@@ -1,5 +1,4 @@
 
-
 package com.cvgs.androidmodule1.finalexercise
 
 import android.os.Bundle
@@ -16,15 +15,13 @@ import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.isNotFilled
 import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.markAsInvalid
 import com.cvgs.androidmodule1.homework.salvadorchavez.tarea1.utils.resetOnTyping
 
-
 class SignUpFragment : Fragment() {
-
-    private lateinit var binding : FragmentSignUpBinding
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -32,37 +29,44 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Asignar hints desde strings.xml
+        binding.etName.hint = getString(R.string.name_hint)
+        binding.etLastName.hint = getString(R.string.last_name_hint)
+        binding.etEmail.hint = getString(R.string.email_hint)
+        binding.etPassword.hint = getString(R.string.password_hint)
+        binding.etPassword2.hint = getString(R.string.repeat_password_hint)
+        binding.btnSignIn.text = getString(R.string.create_account)
+
         binding.etName.resetOnTyping()
         binding.etLastName.resetOnTyping()
         binding.etEmail.resetOnTyping()
         binding.etPassword.resetOnTyping()
         binding.etPassword2.resetOnTyping()
 
-        binding.btnSignIn.setOnClickListener{
+        binding.btnSignIn.setOnClickListener {
             var isValid = true
             val selectedId = binding.rgSex.checkedRadioButtonId
-            var sex = String()
+            var sex = ""
 
-            if (binding.etName.isNotFilled()){
+            if (binding.etName.isNotFilled()) {
                 binding.etName.markAsInvalid()
                 isValid = false
             }
-            if (binding.etLastName.isNotFilled()){
+            if (binding.etLastName.isNotFilled()) {
                 binding.etLastName.markAsInvalid()
                 isValid = false
             }
-            if (binding.etEmail.isNotFilled()){
+            if (binding.etEmail.isNotFilled()) {
                 binding.etEmail.markAsInvalid()
                 isValid = false
             }
-            if (binding.etPassword.isNotFilled()){
-                isValid = false
+            if (binding.etPassword.isNotFilled()) {
                 binding.etPassword.markAsInvalid()
-
-            }
-            if (binding.etPassword2.isNotFilled()){
                 isValid = false
+            }
+            if (binding.etPassword2.isNotFilled()) {
                 binding.etPassword2.markAsInvalid()
+                isValid = false
             }
             if (selectedId == -1) {
                 isValid = false
@@ -71,23 +75,18 @@ class SignUpFragment : Fragment() {
                 sex = selectedRadioButton.text.toString()
             }
 
-
-            if(!isValid){
-                //Toast.makeText(requireContext(),"Llena los campos faltantes", Toast.LENGTH_SHORT).show()
-                val message = """Favor de llenar los datos faltantes """.trimIndent()
+            if (!isValid) {
                 val alertDialog = AlertDialog.Builder(requireContext())
-                    .setTitle("Datos incompletos")
-                    .setMessage(message)
-                    .setPositiveButton("OK") { dialog, which -> }
+                    .setTitle(getString(R.string.error_data))
+                    .setMessage(getString(R.string.incomplete_data))
+                    .setPositiveButton("OK", null)
                     .create()
                 alertDialog.show()
-            }else if (binding.etPassword.text.toString() != binding.etPassword2.text.toString()){
-                //Toast.makeText(requireContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
-                val message = """Las contaseñas no coinciden """.trimIndent()
+            } else if (binding.etPassword.text.toString() != binding.etPassword2.text.toString()) {
                 val alertDialog = AlertDialog.Builder(requireContext())
-                    .setTitle("Error al introducir los datos")
-                    .setMessage(message)
-                    .setPositiveButton("OK") { dialog, which ->
+                    .setTitle(getString(R.string.error_data))
+                    .setMessage(getString(R.string.password_mismatch))
+                    .setPositiveButton("OK") { _, _ ->
                         binding.etPassword.setText("")
                         binding.etPassword2.setText("")
                         binding.etPassword.markAsInvalid()
@@ -95,10 +94,7 @@ class SignUpFragment : Fragment() {
                     }
                     .create()
                 alertDialog.show()
-
-
-            }
-            else {
+            } else {
                 val user = UserEntity(
                     binding.etName.text.toString(),
                     binding.etLastName.text.toString(),
@@ -107,16 +103,16 @@ class SignUpFragment : Fragment() {
                     sex
                 )
                 val message = """
-            Nombre : ${user.name}
-            Apellido: ${user.lastName}
-            E-Mail:  ${user.email}
-            Genero: ${user.gender}
-        """.trimIndent()
+                    ${getString(R.string.name_hint)}: ${user.name}
+                    ${getString(R.string.last_name_hint)}: ${user.lastName}
+                    ${getString(R.string.email_hint)}: ${user.email}
+                    ${getString(R.string.gender_other)}: ${user.gender}
+                """.trimIndent()
 
                 val alertDialog = AlertDialog.Builder(requireContext())
-                    .setTitle("Los datos que ingresaste son:")
+                    .setTitle(getString(R.string.user_info_title))
                     .setMessage(message)
-                    .setPositiveButton("Enviar") { dialog, which ->
+                    .setPositiveButton(getString(R.string.btn_send)) { _, _ ->
                         val bundle = Bundle()
                         bundle.putSerializable("EXTRA_USER", user)
                         val fragment = LoginFragment()
@@ -125,23 +121,18 @@ class SignUpFragment : Fragment() {
                             .replace(R.id.fragmentContainer, fragment)
                             .addToBackStack("LoginFragment")
                             .commit()
-                        Toast.makeText(requireContext(), "Gracias, ahora puedes ingresar tu usuario  y contraseña", Toast.LENGTH_LONG).show()
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.no_account),
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
-                    .setNegativeButton("Cancelar", null)
+                    .setNegativeButton(getString(R.string.btn_cancel), null)
                     .create()
                 alertDialog.show()
-
-                /*Toast.makeText(requireContext(), "$registeredUser", Toast.LENGTH_LONG).show()
-                val intent = Intent(requireContext(), StartUpActivity::class.java).apply {
-                    putExtra("EXTRA_USER", user)
-                }
-                startActivity(intent)*/
-
             }
         }
     }
-
-
 
     companion object {
         @JvmStatic
